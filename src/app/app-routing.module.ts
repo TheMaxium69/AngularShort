c23aa11ce153758ada45f3c4716ac1ac25b887ed
TheMaximeSan <mxm.tournier@gmail.com>
2021-08-03 10:21:41 +0200

    Route finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {ShopComponent} from "./shop/shop.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'shop', component: ShopComponent },
+  {path: '', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -12,4 +12,38 @@ export class ShopComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  shortsTest = [
+    {
+      "id": 1,
+      "name": "jesuisunnom",
+      "couleur": "jesuisunecouleur",
+      "taille": "jesuisunetaille"
+    },
+    {
+      "id": 2,
+      "name": "jesuisunnom",
+      "couleur": "jesuisunecouleur",
+      "taille": "jesuisunetaille"
+    },
+    {
+      "id": 3,
+      "name": "jesuisunnom",
+      "couleur": "jesuisunecouleur",
+      "taille": "jesuisunetaille"
+    },
+    {
+      "id": 4,
+      "name": "jesuisunnom",
+      "couleur": "jesuisunecouleur",
+      "taille": "jesuisunetaille"
+    },
+    {
+      "id": 6,
+      "name": "jesuisunnom2",
+      "couleur": "jesuisunecouleur2",
+      "taille": "jesuisunetaille2"
+    }
+  ];
+
+
 }
