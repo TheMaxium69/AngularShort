b3ea83a881a2f863244d116376d4e559ba194b97
TheMaximeSan <mxm.tournier@gmail.com>
2021-08-03 17:04:36 +0200

    ngfor panier

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanierComponent implements OnInit {
 
+  panier = [{"id":1,"name":"jesuisunnom","couleur":"jesuisunecouleur","taille":"jesuisunetaille"},{"id":7,"name":"zaezaeza","couleur":"azeza","taille":"azezae"},{"id":8,"name":"zaezaeza","couleur":"azeza","taille":"azezae"}];
+
   constructor() { }
 
   ngOnInit(): void {
